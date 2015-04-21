876ae450ff099ee5dc4ec28446bab5a05b5f6455
Luís Dias <lcdias127@gmail.com>
2015-04-21 16:52:20 +0100

    transactions completed

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,43 @@
 class TransactionsController < ApplicationController
   	before_action :set_movie
-
-  	def new
-  		@client_token = Braintree::ClientToken.generate
+    before_filter :authenticate_user!
+  	
+    def new
+      @client_token = Braintree::ClientToken.generate(:customer_id => current_user.customer_id)
+      respond_to do |format|
+        format.html
+      end
   	end
 
   	def create
+      respond_to do |format|
+        # create Braintree customer if still not created
+        unless current_user.customer_id
+          result = Braintree::Customer.create( 
+            :email => current_user.email, 
+            :payment_method_nonce => params[:payment_method_nonce] 
+          )
+          if result.success?
+            current_user.update(customer_id: result.customer.id)
+          else
+            p result.errors
+            format.html {redirect_to purchase_movie_path, notice: "Error creating customer account"}
+          end
+        end
+
+        # Perform transaction for the current customer
+        result = Braintree::Transaction.sale(
+          :customer_id => current_user.customer_id,
+          :amount => @movie.price
+        )
+
+        if result.success?
+          format.html {redirect_to movies_path, notice: "Movie successfully purchased"} 
+        else
+          p result.errors
+          format.html {redirect_to purchase_movie_path, notice: "Error performing transaction"}
+        end        
+      end      
   	end
 
   	private
